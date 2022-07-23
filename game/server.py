91875d564d13efe2e054fd4ff6d8184ee3ed0404
gvsa123 <gvsa123@gmail.com>
2022-07-22 22:24:10 -0600

    Fixed wrong key in ACTIVE_CONNECTIONS. Server now adds active, and removes closed connections.

--- game/server.py
+++ game/server.py
@@ -1,5 +1,8 @@
+"""Websocket server script."""
+
 import asyncio
 import json
+
 import websockets
 
 ACTIVE_CONNECTIONS = set()
@@ -7,25 +10,33 @@ ACTIVE_CONNECTIONS = set()
 
 async def connection_handler(websocket: websockets) -> websockets:
     """
-    Websocket connection handler
+    Websocket connection handler.
 
     TODO:
     - wrong key stored in ACTIVE_CONNECTIONS; new connection being made
     """
-
     try:
         async for payload in websocket:
             message = json.loads(payload.decode('utf-8'))
-            ACTIVE_CONNECTIONS.add(message['websocket_id'])
-            print(ACTIVE_CONNECTIONS)
-            await websocket.send(f"Received: {payload}")
+            if message['websocket_id'] not in ACTIVE_CONNECTIONS:
+                ACTIVE_CONNECTIONS.add(message['websocket_id'])
+                print(f"Added {message['websocket_id']}.")
+                print(f"ACTIVE_CONNECTIONS count: {len(ACTIVE_CONNECTIONS)}")
+            await websocket.send(f"Received: {message}")
 
     except websockets.exceptions.ConnectionClosed:
         print(f"Client {websocket.id} disconnected.")
-        ACTIVE_CONNECTIONS.remove(str(websocket.id))
+        ACTIVE_CONNECTIONS.remove(message['websocket_id'])
+        print(f"Removed {websocket.id}.")
+
+        pass
+    except websockets.exceptions.ConnectionClosedOK:
+        print(f"Client {websocket.id} disconnected.")
+        pass
 
 
 async def main():
+    """Initialize websocket server."""
     async with websockets.serve(connection_handler, "127.0.0.1", 8765):
         await asyncio.Future()  # run forever
 
