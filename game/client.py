0412a64fb5b369d3d6eeaf052b472a258146d18e
gvsa123 <gvsa123@gmail.com>
2022-07-24 07:57:00 -0600

    Fixed client not getting removed from active connections. Exeption handling. Decoupled server logic and added helper functions.

--- game/client.py
+++ game/client.py
@@ -1,16 +1,22 @@
-from typing import Dict
+"""Websocket Client side script."""
 
 import asyncio
 import json
+import random
+from typing import Dict
+
 import websockets
 
 
 class WebSocketClient():
-    """Websocket client"""
+    """Websocket client."""
+
     def __init__(self) -> None:
+        """Initialize class with url variables."""
         self.ws_url: str = "ws://127.0.0.1:"
         self.ws_port: int = 8765
         self.uri = f"{self.ws_url}{self.ws_port}"
+        self.delay = 1
 
     def create_payload(self, WEBSOCKET_ID: str, MESSAGE: str) -> Dict:
         """Construct payload."""
@@ -24,30 +30,30 @@ class WebSocketClient():
         return json.dumps(payload).encode('utf-8')
 
     def create_message(self) -> str:
-        """Create a message to be included in payload"""
-        return input("Please enter your message: ")
+        """Create a message to be included in payload."""
+        return random.randint(1, 5)
 
     async def establish_connection(self) -> None:
-        """
-        Connect to server
-
-        TODO:
-        - issue with keep-alive; where to send ping?
-        """
+        """Establish connection to server."""
         try:
             async with websockets.connect(self.uri) as websocket:
+                WEBSOCKET_ID = str(websocket.id)
+                print(f"Client {websocket.id} connected.")
 
                 while True:
-                    print(f"Client {websocket.id} connected.")
-                    WEBSOCKET_ID = str(websocket.id)
-                    await websocket.send(self.create_payload(WEBSOCKET_ID, MESSAGE=self.create_message()))
+                    await websocket.send(self.create_payload(
+                        WEBSOCKET_ID,
+                        MESSAGE=self.create_message()
+                    ))
+                    await asyncio.sleep(self.delay)
+                    _ = await websocket.recv()
+                    print(_)
 
         except websockets.exceptions.ConnectionClosedError as e:
             print(f"Error: {e}.")
 
         finally:
-            _ = await websocket.recv()
-            print(_)
+            await websocket.close()
 
 
 client = WebSocketClient()
--- game/server.py
+++ game/server.py
@@ -8,46 +8,110 @@ import websockets
 ACTIVE_CONNECTIONS = set()
 
 
-def remove_inactive_connection(websocket_id) -> None:
-    """Removes disconnected websocket connections from ACTIVE_CONNECTIONS."""
-    print(f"Client {websocket_id} disconnected.")
-    ACTIVE_CONNECTIONS.remove(websocket_id)
-    print(f"Removed {websocket_id}.")
+def store_new_connection(websocket: websockets) -> None:
+    """Add new websocket connections to ACTIVE_CONNECTIONS."""
+    if websocket.open:
+        ACTIVE_CONNECTIONS.add(websocket)
+        print(f"Connection from {websocket.id} received.")
+    count_active_connections()
 
 
-def add_new_connection(websocket_id) -> None:
-    """Adds new websocket connections to ACTIVE_CONNECTIONS."""
-    ACTIVE_CONNECTIONS.add(websocket_id)
-    print(f"Added {websocket_id}.")
-    print(f"ACTIVE_CONNECTIONS count: {len(ACTIVE_CONNECTIONS)}")
+def remove_inactive_connection(websocket: websockets) -> None:
+    """Remove disconnected websocket connections from ACTIVE_CONNECTIONS."""
+    if websocket.closed:
+        print(f"Client {websocket.id} disconnected.")
+        ACTIVE_CONNECTIONS.remove(websocket)
+        count_active_connections()
+
+
+def check_active_connections() -> None:
+    """
+    Manually check if all stored connections are active.
+
+    TODO:
+    - RuntimeWarning: Enable tracemalloc to get the object allocation traceback
+    """
+    for connection in ACTIVE_CONNECTIONS:
+        if connection.open:
+            pass
+        else:
+            remove_inactive_connection(connection)
+
+
+def count_active_connections() -> None:
+    """Return count of all active connections."""
+    if len(ACTIVE_CONNECTIONS) > 0:
+        print(f"Active connections: {len(ACTIVE_CONNECTIONS)}")
+    else:
+        print("No active connections.")
+
+
+def decode_json_payload(payload) -> json:
+    """Decode incoming message."""
+    return json.loads(payload.decode('utf-8'))
 
 
 async def connection_handler(websocket: websockets) -> websockets:
-    """Websocket connection handler."""
-    try:
-        async for payload in websocket:
-            message = json.loads(payload.decode('utf-8'))
-            websocket_id = message['websocket_id']
+    """
+    Websocket connection handler.
 
-            if websocket_id not in ACTIVE_CONNECTIONS:
-                add_new_connection(websocket_id)
-            await websocket.send(f"Received: {message}")
+    Receives:
+        "websocket_id"
+        "start_game"
+        "stop_game"
+        "msg"
+    """
+    try:
+        async for _ in websocket:
+
+            if websocket not in ACTIVE_CONNECTIONS:
+                store_new_connection(websocket)
+
+            await asyncio.sleep(.5)
+            if websocket.open:
+                await websocket.send("message['msg']")
+            else:
+                await websocket.close()
+                remove_inactive_connection(websocket)
 
     except websockets.exceptions.ConnectionClosed:
-        remove_inactive_connection(websocket_id)
+        remove_inactive_connection(websocket)
+        print("ConnectionClosed.")
         pass
 
     except websockets.exceptions.ConnectionClosedOK:
-        remove_inactive_connection(websocket_id)
+        remove_inactive_connection(websocket)
+        print("ConnectionClosedOK.")
         pass
+
+    except websockets.exceptions.ConnectionClosedError:
+        remove_inactive_connection(websocket)
+        print("ConnectionClosedError")
+        pass
+
+    except asyncio.exceptions.CancelledError:
+        print("CancelledError")
+        pass
+
+    finally:
+        try:
+            check_active_connections()
+        except RuntimeError:
+            check_active_connections()
 
 
 async def main():
     """Initialize websocket server."""
-    async with websockets.serve(connection_handler, "127.0.0.1", 8765):
+    async with websockets.serve(
+        connection_handler,
+        "127.0.0.1",
+        8765,
+        ping_timeout=5,
+        ping_interval=5
+    ):
+        print("Websocket server running.")
+        count_active_connections()
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
-    if connection_handler:
-        print("Websocket server running.")
     asyncio.run(main())
