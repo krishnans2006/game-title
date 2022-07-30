1b962e76ef3db69c717e9454558d50dcb50d429d
gvsa123 <gvsa123@gmail.com>
2022-07-30 15:47:09 -0600

    Got game_thread running from main.py. Got server_thread running from main.py. Got client_thread running from main.py - player attributes from player.Player.to_dict() needs to be passed to WebSocketClient instance as part of playload.

--- client/game.py
+++ client/game.py
@@ -1,13 +1,19 @@
 import asyncio
+import logging
 import sys
+import threading
 
 import pygame
 
+# from client_connector import WebSocketClient
+from client import client_connector
 from client import config as c
-from client.connection import Connection
 from client.state import IPPrompt, PlayGame, StartMenu
+from server import server_connector
 
-connection: Connection = Connection()
+# connection: Connection = Connection()
+# connection = server_connector.main()
+
 
 pygame.init()
 
@@ -42,7 +48,18 @@ def redraw(window: pygame.Surface, game_state: str):
 
 
 async def main():
-    """Runs the main game loop."""
+    """
+    Runs the main game loop.
+
+    game_state needs to be set to game in order for gui to run.
+    game_state is temporarily changed to get user_input in order
+    to run either server_thread or client_thread.
+
+    server_thread needs to be running before an instance of client_thread
+    can connect.
+
+    client_thread can still be run but without server connection.
+    """
     game_state: str = "menu"
 
     game_running = True
@@ -52,14 +69,40 @@ async def main():
             if event.type == pygame.QUIT:
                 game_running = False
 
-        if run_result := state_map[game_state].update(events):
+        if run_result := state_map[game_state].update(events):  # insert check is_server_running
             if run_result == "server":
-                # Start server as subprocess
-                print("Started server")
+                print(f"user input: {run_result}")
+
+                # sets game_state. game_state needs to be game.
                 game_state = "game"
+
+                server_thread = threading.Thread(
+                    target=asyncio.run, args=(server_connector.main(),)
+                )
+                logging.info("Main    : before running server_thread")
+                server_thread.start()
+                logging.info("Main    : server_thread running.")
+
+                print(server_thread)
+                print(type(server_thread))
+
+            elif run_result == "ip":
+                print(f"user input: {run_result}")
+
+                # sets game_state. game_state needs to be game.
+                game_state = "game"
+
+                # run a client instance with game.
+                client_thread = threading.Thread(
+                    target=asyncio.run, args=(client_connector.run(),)
+                )
+                logging.info("Main    : before running client_thread")
+                client_thread.start()
+                logging.info("Main    : client_thread running.")
+
             else:
-                game_state = run_result  # Change game state
-            print(f"game_state: {game_state}")
+                print(f"run_result: {run_result}")
+                game_state = run_result  # make sure game runs.
         redraw(win, game_state)
         clock.tick(60)
 
--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import pygame
 
 from client import game  # noqa: F401
 from client.client_connector import WebSocketClient  # ugly import statement
-from server import server_connector
 
 
 def create_client():
@@ -32,7 +31,7 @@ def create_client():
 
 async def run_game():
     """Runs the game instance."""
-    game.main()
+    await game.main()
     pygame.quit()
     sys.exit()
 
@@ -44,22 +43,25 @@ if __name__ == "__main__":
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
     logging.info("Main    : before creating thread")
-    server_thread = threading.Thread(target=asyncio.run, args=(server_connector.main(),))
-    logging.info("Main    : before running server_thread")
-    server_thread.start()
+    # server_thread = threading.Thread(target=asyncio.run, args=(server_connector.main(),))
+    game_thread = threading.Thread(target=asyncio.run, args=(run_game(),))
+    logging.info("Main    : before running game_thread")
+    game_thread.start()
+    logging.info("Main    : game_thread running.")
 
     time.sleep(2)  # Wait for server to start.
 
-    logging.info("Main    : before running client_thread")
-    client_thread = threading.Thread(target=create_client())
-    client_thread.start()
+    # client_thread = threading.Thread(target=create_client())
+    # client_thread.start()
 
-    logging.info("Main    : before running game_thread")
-    game_thread = threading.Thread(
-        target=asyncio.run, args=(run_game())
-    )  # Opens window but does not run game - blank screen.
-    game_thread.start()
+    # logging.info("Main    : before running game_thread")
+    # game_thread = threading.Thread(
+    #     target=asyncio.run, args=(run_game())
+    # )  # Opens window but does not run game - blank screen.
+    # game_thread.start()
 
-    logging.info("Main    : wait for the thread to finish")
-    # x.join()
-    logging.info("Main    : all done")
+    # logging.info("Main    : wait for the thread to finish")
+    # # x.join()
+    # logging.info("Main    : all done")
+
+    # asyncio.run(run_game())
